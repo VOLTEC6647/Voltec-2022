08874a4797e038d6ad9375582714547622e4bac3
PolloGunpao <70779495+PolloGunpao@users.noreply.github.com>
2022-02-03 17:45:03 -0600

    Hola

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
+  //holiwis
   /** Creates a new Shooter. */
   public ShooterSubsystem() {}
 
