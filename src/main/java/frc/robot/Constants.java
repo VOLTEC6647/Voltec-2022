952c8a17a35bf9791dba5c1daf2550e59cbcf3eb
PolloGunpao <70779495+PolloGunpao@users.noreply.github.com>
2022-02-03 19:27:46 -0600

    Motores importados

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,10 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class ShooterConstants{
+        public static final int deliveryID = 0;
+        public static final int shooterID = 1;
+        public static final int counterspinID = 3;
+    }
+}
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,12 +1,21 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+//HOLAAAAAA
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
+
 
 public class ShooterSubsystem extends SubsystemBase {
+  CANSparkMax delivery = new CANSparkMax(ShooterConstants.deliveryID, MotorType.kBrushless);
+  CANSparkMax shooter = new CANSparkMax(ShooterConstants.shooterID, MotorType.kBrushless);
+  CANSparkMax counterspin = new CANSparkMax(ShooterConstants.counterspinID, MotorType.kBrushless);
   //holiwis
   /** Creates a new Shooter. */
   public ShooterSubsystem() {}
