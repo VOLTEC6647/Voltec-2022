b72b06cae6c49027faa3d330b46f1ce6d299b9c0
ianyan1 <MY_NAME@example.com>
2022-02-15 01:13:41 -0600

    Avances Shoooter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,8 +14,44 @@ package frc.robot;
  */
 public final class Constants {
     public static final class ShooterConstants{
-        public static final int deliveryID = 0;
         public static final int shooterID = 1;
-        public static final int counterspinID = 3;
+        public static final int counterSpinID = 3;
+
+        //Shooter PID
+        public static final double shooterkP = 6e-5; 
+        public static final double shooterkI = 0;
+        public static final double shooterkD = 0; 
+        public static final double shooterkIz = 0; 
+        public static final double shooterkFF = 0.000015; 
+
+        //CounterSpin PID
+        public static final double counterSpinkP = 6e-5; 
+        public static final double counterSpinkI = 0;
+        public static final double counterSpinkD = 0; 
+        public static final double counterSpinkIz = 0; 
+        public static final double counterSpinkFF = 0.000015; 
+
+        public static final int shooterFender = 2500; 
+        public static final int backSpinFender = 2500; 
+        public static final int shooter1MeterFender = 3000; 
+        public static final int shooter1backSpinFender = 3000; 
+        public static final int velocityTolerance = 50; 
+    }
+
+    public static final class DeliveryConstants{
+        public static final int deliveryID = 0;
+        //Delivery PID
+        public static final double deliverykP = 6e-5; 
+        public static final double deliverykI = 0;
+        public static final double deliverykD = 0; 
+        public static final double deliverykIz = 0; 
+        public static final double deliverykFF = 0.000015;
+
+        public static final int deliveryRot = 1; 
+        public static final double errorTolerance = .5; 
+        
+    }
+    public static final class OIConstants{
+        public static final int KDriverControllerPort = 0;
     }
 }
